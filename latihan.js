40db1eddc1713ea3620a91aff1d900ca8669959a
Putra Adhli Falah <71480@fif.co.id>
2025-06-24 14:55:40 +0700

    javascript algorithms

--- latihan.js
+++ latihan.js
@@ -61,3 +61,57 @@ console.log(age >= 18 && age<=55 ?
 // while (input !== 'exit') {
 //     input = prompt("Type something (or exit to quit):");
 // }
+
+let object = {
+    name: "adhli",
+    age: 25,
+    isOnLeave: true,
+    doYourJob: function(){
+        if (this.isOnLeave){
+            return 'mentoring'
+        } else {
+            return 'front end'
+        }
+    }
+}
+
+console.log(object.doYourJob());
+
+
+// function Kreditur(name, penghasilan){
+//     this.nama = name;
+//     this.income = penghasilan;
+// }
+
+// const KrediturOne = new Kreditur('John',1000);
+// const KrediturTwo = new Kreditur('Doe',1000);
+
+// console.log(KrediturOne);
+// console.log(KrediturTwo);
+
+const {name, ...otherInfo} = object;
+console.log(otherInfo);
+
+const user = {namef: 'adhli', age: 28};
+const location = {city: "jakarta"};
+
+// const profile = {...user, ...location};
+
+// console.log(profile);
+
+const { namef } = user;
+const { city } = location;
+
+const result = {...{namef}, ...{city}};
+
+console.log(result);
+
+//aray manipulation
+const fruit = ['banana', 'apple', 'orange']
+//buat masukin elemen ke array indeks terakhir
+// fruit.push('kiwi'); 
+//masukin elemen ke array indeks pertama
+// fruit.unshift('kiwi');
+//                      parameter baru  ubah parameter tersebut jadi sesuai sama variabel baru
+const result_fruit = fruit.find(f => f === "apple");
+console.log(result_fruit);
